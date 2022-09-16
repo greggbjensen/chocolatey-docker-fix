d9aee39298416dbc0caf7fe5ef61c30a19cfe131
Gregg B. Jensen <greggbjensen@users.noreply.github.com>
2022-09-16 09:10:16 -0600

    Added script to create updated package.

---
+++ buildPackage.ps1
@@ -0,0 +1,20 @@
+$packageName = 'chocolatey.1.1.0'
+$packagePath = "$PSScriptRoot\dist\$packageName.nupkg"
+
+Write-Host "Cleaning package."
+if (Test-Path $packagePath) {
+    Remove-Item $packagePath
+}
+
+Write-Host "Compressing archive."
+$compress = @{
+    Path = "$PSScriptRoot\src\$packageName"
+    CompressionLevel = "Fastest"
+    DestinationPath = "$PSScriptRoot\dist\$packageName.zip"
+}
+Compress-Archive @compress
+
+Write-Host "Renaming package."
+Rename-Item "$PSScriptRoot\dist\$packageName.zip" $packagePath
+
+Write-Host "Update complete." -ForegroundColor Green
